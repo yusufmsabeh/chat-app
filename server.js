ccaf07696620bfb3a294c05527ff0bb65e132098
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-19 18:09:35 +0300

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+//server.js
+const application = require('express')();
+const server = require('http').createServer(application)
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000
+application.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+server.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
+});
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        console.log('User is disconnected - Username: ' + socket.username);
+    });
+    socket.on('new message', (msg) => {
+        io.emit('send message', {message: msg, user: socket.username});
+    });
+    socket.on('new user', (usr) => {
+        socket.username = usr;
+        console.log('User is connected - Username: ' + socket.username);
+    });
+});
+
+
